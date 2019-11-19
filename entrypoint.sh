f319e850afe54720bf0f8ddc26596ba9e5385753
Danny Haynes <dhaynes@mitre.org>
2019-11-19 07:30:32 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,17 +73,17 @@ dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
 
 #echo "Commit: $commit"
 #echo "--------START--------"
-git pull origin master
-git config --global user.name "djhaynes"
-git config --global user.email "dhaynes@mitre.org"
-echo "**Release:** " > VERSION.md
-echo "$new" >> VERSION.md
+#git pull origin master
+#git config --global user.name "djhaynes"
+#git config --global user.email "dhaynes@mitre.org"
+#echo "**Release:** " > VERSION.md
+#echo "$new" >> VERSION.md
 #echo "<br><br>**Date:** " >> VERSION.md
 #echo "$dt" >> VERSION.md
 #echo "<br><br>**Commit:** " >> VERSION.md
 #echo "$commit" >> VERSION.md
-git commit -m "Updating VERSION.md" VERSION.md
-git push origin HEAD:master
+#git commit -m "Updating VERSION.md" VERSION.md
+#git push origin HEAD:master
 #ls
 #cat VERSION.md
 # printf "module InspecTools\n  VERSION = '$(git describe --tags $(git rev-list --tags --max-count)'.freeze\nend\n" > lib/inspec_tools/version.rb
