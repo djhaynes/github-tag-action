b1af1e1bb3be0884a1b6e8e39b91ec3e5974dab5
Danny Haynes <dhaynes@mitre.org>
2019-11-19 10:17:24 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-echo "HELLO"
+# Get current tag number
+$tag = $(git describe --tags $(git rev-list --tags --max-count=1))
+
+# if there are none, start tags at 0.0.0
+if [ -z "$tag" ]
+then
+    log=$(git log --pretty=oneline)
+    tag=0.0.0
+else
+    log=$(git log $tag..HEAD --pretty=oneline)
+fi
+
+# get commit logs and determine home to bump the version
+# supports #major, #minor, #patch (anything else will be 'minor')
+case "$log" in
+    *#major* ) new=$(semver bump major $tag);;
+    *#minor* ) new=$(semver bump minor $tag);;
+    *#patch* ) new=$(semver bump patch $tag);;
+    * ) new=$(semver bump `echo $default_semvar_bump` $tag);;
+esac
+
+# prefix with 'v'
+if $with_v
+then
+    new="v$new"
+fi
+
+if $pre_release
+then
+    new="$new-${commit:0:7}"
+fi
+
+echo "$new"
